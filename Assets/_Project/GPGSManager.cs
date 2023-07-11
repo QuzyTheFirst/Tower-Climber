ca02ce7a7b95320deba15256dcbe5c85d14a47e2
Quzy <40630448+QuzyTheFirst@users.noreply.github.com>
2023-07-11 15:12:14 +0300

    Added Achievements

--- Assets/_Project/GPGSManager.cs
+++ Assets/_Project/GPGSManager.cs
@@ -6,13 +6,29 @@ using GooglePlayGames.BasicApi;
 
 public class GPGSManager : MonoBehaviour
 {
+    private Achievements _achievements;
+
     private void Start()
     {
+        _achievements = GetComponent<Achievements>();
+
+        PlayGamesPlatform.Activate();
         PlayGamesPlatform.Instance.Authenticate(AunthenticateCallback);
     }
 
     internal void AunthenticateCallback(SignInStatus status)
     {
-        Debug.Log(status);
+        switch (status) 
+        {
+            case SignInStatus.Success:
+                Debug.Log("<color=cyan>Successfully entered Google Play Services</color>");
+                break;
+            case SignInStatus.Canceled:
+                Debug.Log("<color=cyan>Google Play Services canceled connection</color>");
+                break;
+            case SignInStatus.InternalError:
+                Debug.Log("<color=cyan>Internal error while trying to sign in to Google Play Services</color>");
+                break;
+        }
     }
 }
